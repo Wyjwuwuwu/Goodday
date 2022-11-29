3efb8e9a8d6b33e47e763d4ecda7ae411e762139
wuyijin <1292946453@qq.com>
2022-11-29 21:22:48 +0800

    Revert "Merge branch 'yuxuan_branch'"
    This reverts commit a2099dea40b10af7631bfab11db4aa2452de095e, reversing
    changes made to 787185df02d59cf2c64de5e4fe663c7d7ca4bca9.

--- app/src/main/java/com/example/goodday/MainActivity.kt
+++ app/src/main/java/com/example/goodday/MainActivity.kt
@@ -2,33 +2,24 @@ package com.example.goodday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.WindowManager
-import androidx.core.view.GravityCompat
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
-
 class MainActivity : AppCompatActivity() {
-
-    lateinit var bottomNav : BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getWindow().setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
         setContentView(R.layout.activity_main)
-
-        //Initialize the bottom navigation view
-        //create bottom navigation view object
-        val bottomNavigationView = findViewById<BottomNavigationView
-                >(R.id.bottom_nav)
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         val navController = findNavController(R.id.main_fragment)
-        bottomNavigationView.setupWithNavController(
-            navController
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment,R.id.newsFragment)
         )
-    }
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        bottomNav.setupWithNavController(navController)
+        getSupportActionBar()?.hide();
 
+    }
 }
--- app/src/main/java/com/example/goodday/ProfileFragment.kt
+++ app/src/main/java/com/example/goodday/ProfileFragment.kt
@@ -1,92 +1,20 @@
 package com.example.goodday
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.Fragment
-import com.google.android.material.navigation.NavigationView
-
 
 class ProfileFragment : Fragment() {
 
-    lateinit var drawerLayout : DrawerLayout
-    lateinit var navigationView: NavigationView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
-
-        drawerLayout = rootView.findViewById(R.id.drawer_layout)
-        navigationView = rootView.findViewById(R.id.nav_view)
-        val toolbar = rootView.findViewById<Toolbar>(R.id.toolbar)
-
-        (activity as AppCompatActivity?)!!.setSupportActionBar(toolbar)
-
-        val menu = navigationView.menu
-        menu.findItem(R.id.nav_logout).setVisible(true)
-
-        navigationView.bringToFront()
-        val toggle = ActionBarDrawerToggle(
-            requireActivity(),
-            drawerLayout,
-            toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
-
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_index -> {}
-                R.id.nav_record -> {
-                }
-                R.id.nav_blood_stand -> {
-                }
-                R.id.nav_history -> {
-                }
-                R.id.nav_treatment -> {
-                }
-                R.id.nav_logbook -> {}
-                R.id.nav_setting -> {}
-                R.id.nav_edit -> {}
-                R.id.nav_logout -> {
-                    confirm_logout()
-                }
-                R.id.nav_share -> {}
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
         // Inflate the layout for this fragment
-        return rootView
-    }
-
-
-    fun confirm_logout() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setCancelable(true)
-        builder.setTitle("Logout")
-        builder.setMessage("Are you sure you want to logout?")
-        builder.setPositiveButton(
-            "Confirm"
-        ) { dialog, which -> }
-        builder.setNegativeButton(
-            android.R.string.cancel
-        ) { dialog, which -> }
-        val dialog = builder.create()
-        dialog.show()
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
 }
