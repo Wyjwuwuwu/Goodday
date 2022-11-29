051b4aab965de8186b37d24fd3927340ce8ca42d
wuyijin <1292946453@qq.com>
2022-11-29 21:22:31 +0800

    Revert "Merge branch 'master' of https://github.com/Wyjwuwuwu/Goodday"
    This reverts commit 2bd90f4062780da9db85c688b0f5189991b85f1f, reversing
    changes made to a2099dea40b10af7631bfab11db4aa2452de095e.

--- app/src/main/java/com/example/goodday/ArticleActivity.kt
+++ app/src/main/java/com/example/goodday/ArticleActivity.kt
@@ -1,22 +1,12 @@
 package com.example.goodday
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 
 class ArticleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_article)
         getSupportActionBar()?.hide()
-
-
-
-        val back: ImageButton = findViewById<ImageButton>(R.id.ivReturnArticle)
-        back.setOnClickListener {
-            val intent = Intent(this@ArticleActivity, NewsInformatinActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
