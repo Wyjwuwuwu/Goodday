bdcd0920edddacb3aab5d49b2ae65446259d7cde
Lyrebird <yuxuanwuu@outlook.com>
2022-11-29 22:34:30 +0800

    Revert "Merge branch 'master' into yuxuan_branch"
    This reverts commit 5ef27aa845b0e6903ba5914766bfa344bc9639ad, reversing
    changes made to 56401204231b15958356f4bc2aee227b02c24b1c.

--- app/src/main/java/com/example/goodday/HomeFragment.kt
+++ app/src/main/java/com/example/goodday/HomeFragment.kt
@@ -1,10 +1,11 @@
 package com.example.goodday
-
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 
 class HomeFragment : Fragment() {
@@ -13,9 +14,37 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        val circularProgressBar = rootView.findViewById<CircularProgressBar>(R.id.circularProgressBar)
+
+        circularProgressBar.apply {
+            // Set Progress
+            //progress = 85f
+            // or with animation
+            setProgressWithAnimation(86f, 1000) // =1s
+
+            // Set Progress Max
+            progressMax = 100f
+
+            // Set ProgressBar Color
+            progressBarColor = Color.rgb(88,139,139)
+
+            // Set background ProgressBar Color
+            backgroundProgressBarColor = Color.rgb(245,245,245)
+
+            // Set Width
+            progressBarWidth = 18f // in DP
+            backgroundProgressBarWidth = 18f // in DP
+
+            // Other
+            roundBorder = true
+            startAngle = 0f
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return rootView
     }
+
 
 
 }
---
+++ app/src/main/java/com/example/goodday/LogbookEdit.kt
@@ -0,0 +1,11 @@
+package com.example.goodday
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogbookEdit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logbook_edit)
+    }
+}
