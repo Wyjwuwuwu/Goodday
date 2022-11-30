6c1a4aa64b5906744342d8dad608dd6ff9c1e1ab
Lyrebird <yuxuanwuu@outlook.com>
2022-11-30 13:59:32 +0800

    check in history and home

--- app/src/main/java/com/example/goodday/HealthView.kt
+++ app/src/main/java/com/example/goodday/HealthView.kt
@@ -21,7 +21,31 @@ class HealthView : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_health_view)
 
+        share = findViewById(R.id.btn_share)
+        text = findViewById(R.id.text)
+
+        share.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                // Now share text only function will be called
+                // here we  will be passing the text to share
+                shareTextOnly(text.getText().toString())
+            }
+        })
     }
 
+    private fun shareTextOnly(titlee: String) {
 
+        // The value which we will sending through data via
+        // other applications is defined
+        // via the Intent.ACTION_SEND
+        val intentt = Intent(Intent.ACTION_SEND)
+
+        // setting type of data shared as text
+        intentt.type = "text/plain"
+        intentt.putExtra(Intent.EXTRA_SUBJECT, "Subject Here")
+
+        // Adding the text to share using putExtra
+        intentt.putExtra(Intent.EXTRA_TEXT, titlee)
+        startActivity(Intent.createChooser(intentt, "Share Via"))
+    }
 }
--- app/src/main/java/com/example/goodday/HomeFragment.kt
+++ app/src/main/java/com/example/goodday/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.goodday
-
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 
 class HomeFragment : Fragment() {
@@ -13,9 +17,45 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+        val circularProgressBar = rootView.findViewById<CircularProgressBar>(R.id.circularProgressBar)
+        val btn_visualization = rootView.findViewById<AppCompatButton>(R.id.btn_visualization)
+        val btn_history = rootView.findViewById<AppCompatButton>(R.id.btn_check)
+
+        btn_history.setOnClickListener {
+            val intent = Intent (activity, HealthView::class.java)
+            activity?.startActivity(intent)
+        }
+
+
+        circularProgressBar.apply {
+            // Set Progress
+            //progress = 85f
+            // or with animation
+            setProgressWithAnimation(86f, 1000) // =1s
+
+            // Set Progress Max
+            progressMax = 100f
+
+            // Set ProgressBar Color
+            progressBarColor = Color.rgb(88,139,139)
+
+            // Set background ProgressBar Color
+            backgroundProgressBarColor = Color.rgb(245,245,245)
+
+            // Set Width
+            progressBarWidth = 18f // in DP
+            backgroundProgressBarWidth = 18f // in DP
+
+            // Other
+            roundBorder = true
+            startAngle = 0f
+            progressDirection = CircularProgressBar.ProgressDirection.TO_RIGHT
+        }
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return rootView
     }
+
 
 
 }
