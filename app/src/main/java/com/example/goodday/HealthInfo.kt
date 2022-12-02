3fdc9bf20d4709ad6be8b4adbd97974e49826075
Lyrebird <yuxuanwuu@outlook.com>
2022-12-02 20:04:04 +0800

    fit screen

--- app/src/main/java/com/example/goodday/HealthInfo.kt
+++ app/src/main/java/com/example/goodday/HealthInfo.kt
@@ -3,6 +3,7 @@ package com.example.goodday
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatImageButton
 
@@ -12,10 +13,22 @@ class HealthInfo : AppCompatActivity() {
         setContentView(R.layout.activity_health_info)
 
         val btn_back = findViewById<AppCompatImageButton>(R.id.btn_back)
+        val btn_article = findViewById<Button>(R.id.btn_article)
+        val btn_video = findViewById<Button>(R.id.btn_video)
 
         btn_back.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+
+        btn_article.setOnClickListener {
+            val intent = Intent(this,NewsInformatinActivity::class.java)
+            startActivity(intent)
+        }
+        btn_video.setOnClickListener {
+            val intent = Intent(this,NewsInformatinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
