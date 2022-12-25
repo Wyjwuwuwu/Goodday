7e6539ecded082e0f4eeef83f2cfa2368d46388e
hyxaa <SWE1909470@xmu.edu.my>
2022-12-25 12:10:08 +0800

    Revert "Merge branch 'master' into hyx"
    This reverts commit 26e56eca8e44cf99056a1c66f49514d1c495eec3, reversing
    changes made to 229d056d9755de3770228b44755ef54412a59735.

--- app/src/main/java/com/example/goodday/VideoPlayActivity.kt
+++ app/src/main/java/com/example/goodday/VideoPlayActivity.kt
@@ -1,75 +1,37 @@
 package com.example.goodday
 
 import android.content.Intent
-import android.net.Uri
-import android.os.Bundle
-import android.widget.MediaController
 import androidx.appcompat.app.AppCompatActivity
-import com.bumptech.glide.Glide
-import com.bumptech.glide.request.RequestOptions
-import com.example.goodday.databinding.ActivityVideoPlayBinding
+import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 
 class VideoPlayActivity : AppCompatActivity() {
-    private var binding: ActivityVideoPlayBinding? = null
-    var videoImage : String? = null
-    var video :String? = null
-    var author: String? = null
-    var authorLink :String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityVideoPlayBinding.inflate(layoutInflater)
-        setContentView(binding!!.root)
+        setContentView(R.layout.activity_video_play)
 
         getSupportActionBar()?.hide()
 
-        val bundle = intent.extras
-        videoImage = bundle!!.getString("videoImage", "Default")
-        video = bundle!!.getString("video", "Default")
-        author = bundle!!.getString("author", "The author is empty")
-        authorLink = bundle!!.getString("authorlink", "The link is empty")
-        Glide.with(this)
-                .load(videoImage)
-                .apply(RequestOptions.centerCropTransform())
-                .into(binding!!.ivVideo)
-        val uri: Uri = Uri.parse(video)
-        binding?.videoView?.setVideoURI(uri)
-        binding?.author?.text = author.toString()
-        binding?.authorlink?.text = authorLink.toString()
+        val video = intent.getParcelableExtra<VidImageModel>("video")
+        if(video!=null)
+        {
+            val textView: TextView = findViewById(R.id.videoTitle)
+            val imageView: ImageView = findViewById(R.id.ivVideo)
+            textView.text = video.VidImageName
+            imageView.setImageResource(video.VidImageImg)
 
-        val mediaController = MediaController(this)
-        mediaController.setAnchorView(binding?.videoView)
-
-        // on below line we are setting media player
-        // for our media controller.
-        mediaController.setMediaPlayer(binding?.videoView)
-
-        // on below line we are setting media
-        // controller for our video view.
-        binding?.videoView?.setMediaController(mediaController)
-
-        // on below line we are
-        // simply starting our video view.
-        binding?.videoView?.start()
-//        val video = intent.getParcelableExtra<NewsVideo>("video")
-
-//        if(video!=null)
-//        {
-//            binding?.videoTitle?.text = video.user?.description.toString()
-//            binding?.authorlink?.text = video.user?.url.toString()
-//            Glide.with(this)
-//                .load(video.urlToImage)
-//                .apply(RequestOptions.centerCropTransform())
-//                .into(binding!!.ivVideo)
-//
-//        }
-
-        binding?.ivBackHomeVideoPlay?.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
         }
 
-        binding?.ivReturnVideo?.setOnClickListener {
+        val back: ImageButton = findViewById<ImageButton>(R.id.ivReturnVideo)
+        back.setOnClickListener {
             finish()
+        }
+        val home: ImageButton = findViewById<ImageButton>(R.id.ivBackHomeVideoPlay)
+        home.setOnClickListener {
+            val intent = Intent(this@VideoPlayActivity, MainActivity::class.java)
+            startActivity(intent)
         }
 
 
