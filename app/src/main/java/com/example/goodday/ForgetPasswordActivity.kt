8a4da65e784ad016af06b51e6272aefd49d7b24c
wuyijin <1292946453@qq.com>
2022-12-31 23:32:46 +0800

    Conflicts

--- app/src/main/java/com/example/goodday/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/goodday/ForgetPasswordActivity.kt
@@ -8,20 +8,13 @@ import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
 import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageButton
-import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
 
 class ForgetPasswordActivity : AppCompatActivity() {
-
-    private lateinit var email: EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
 
-        email = findViewById(R.id.et_forget_email)
         //hide action bar
         if (supportActionBar != null) {
             supportActionBar!!.hide()
@@ -40,22 +33,13 @@ class ForgetPasswordActivity : AppCompatActivity() {
 
         val btnForgetPasswordActivityCheckValidationActivity: Button = findViewById<Button>(R.id.btn_send)
         btnForgetPasswordActivityCheckValidationActivity.setOnClickListener {
-            val email: String = email.text.toString().trim()
-            if (email.isEmpty()){
-                Toast.makeText(this, "Please Enter Your Email Address", Toast.LENGTH_SHORT).show()
-            }else{
-                FirebaseAuth.getInstance().sendPasswordResetEmail(email)
-                    .addOnCompleteListener{task ->
-                        if (task.isSuccessful){
-                            Toast.makeText(this, "Email sent successfully to reset your password !", Toast.LENGTH_LONG).show()
-                            val intent: Intent = Intent(this@ForgetPasswordActivity,LoginActivity::class.java)
-                            startActivity(intent)
+            //explicit intent
+            val intent: Intent = Intent(
+                this@ForgetPasswordActivity,
+                CheckValidationActivity::class.java
+            )
 
-                        }else{
-                            Toast.makeText(this,task.exception!!.message.toString(), Toast.LENGTH_LONG).show()
-                        }
-                    }
-            }
+            startActivity(intent)
         }
 
         val back : View = findViewById<ImageButton>(R.id.back_to)
--- app/src/main/java/com/example/goodday/NewsPicTwoFragment.kt
+++ app/src/main/java/com/example/goodday/NewsPicTwoFragment.kt
@@ -7,31 +7,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
+import com.example.goodday.databinding.FragmentNewsPicTwoBinding
+import com.example.goodday.videoModel.NewsVideo
+import com.example.goodday.videoUI.VideoAdapter
+import com.example.goodday.videoUI.VideoViewModel
 
 class NewsPicTwoFragment : Fragment() {
 
     //Create the image slider
     private lateinit var viewPager_pic_two: ViewPager
     lateinit var viewPagerAdapter: ImageSliderAdapter
-    private lateinit var vidImageAdapter: VidImageAdapter
+//    private lateinit var vidImageAdapter: VidImageAdapter
     lateinit var imageList: List<Int>
 
+    private var binding: FragmentNewsPicTwoBinding? = null
+    var videoArrayList: ArrayList<NewsVideo> = ArrayList<NewsVideo>()
+    var videoAdapter: VideoAdapter? = null
+    private val videoViewModel: VideoViewModel by viewModels()
     // Recycler view
-    private lateinit var VidImageRV: RecyclerView
-    private lateinit var VidImageList: ArrayList<VidImageModel>
-    lateinit var image: Array<Int>
-    lateinit var imageName: Array<String>
+//    private lateinit var VidImageRV: RecyclerView
+//    private lateinit var VidImageList: ArrayList<VidImageModel>
+//    lateinit var image: Array<Int>
+//    lateinit var imageName: Array<String>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_pic_two, container, false)
+        binding = FragmentNewsPicTwoBinding.inflate(layoutInflater)
+        return binding!!.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,24 +52,31 @@ class NewsPicTwoFragment : Fragment() {
             val intent = Intent(view.context, VideoActivity::class.java)
             startActivity(intent)
         }
+        videoViewModel!!.initFragment()
+        videoViewModel!!.getVideoRepository()?.observe(viewLifecycleOwner) { videoResponse ->
+            val newsVideos: List<NewsVideo> = videoResponse.videos!!
+            videoArrayList.addAll(newsVideos)
+            videoAdapter!!.notifyDataSetChanged()
+        }
+        setupRecyclerView()
         // Recycler view
-        image = arrayOf(
-            R.drawable.sport1,R.drawable.sport2,R.drawable.sport3,R.drawable.sport4,R.drawable.sport5
-        )
-        imageName = arrayOf(
-            "Legal,ethical concerns of involving children in medical decision",
-            "Health Hacks, things you need to know",
-            "The best health article",
-            "SUMMARY Of health","Flat world health day poster"
-        )
-
-        VidImageRV = view.findViewById(R.id.recylerView)
-        val layoutManager = GridLayoutManager(view.context, 2)
-        VidImageRV.layoutManager = layoutManager
-
-        VidImageList = arrayListOf<VidImageModel>()
-
-        getUserdata()
+//        image = arrayOf(
+//            R.drawable.sport1,R.drawable.sport2,R.drawable.sport3,R.drawable.sport4,R.drawable.sport5
+//        )
+//        imageName = arrayOf(
+//            "Legal,ethical concerns of involving children in medical decision",
+//            "Health Hacks, things you need to know",
+//            "The best health article",
+//            "SUMMARY Of health","Flat world health day poster"
+//        )
+//
+//        VidImageRV = view.findViewById(R.id.recylerView)
+//        val layoutManager = GridLayoutManager(view.context, 2)
+//        VidImageRV.layoutManager = layoutManager
+//
+//        VidImageList = arrayListOf<VidImageModel>()
+//
+//        getUserdata()
 
         //Create the image slider
         imageList = ArrayList<Int>()
@@ -69,23 +86,43 @@ class NewsPicTwoFragment : Fragment() {
         imageList = imageList + R.drawable.sport5
         imageList = imageList + R.drawable.sport1
 
-
         viewPagerAdapter = ImageSliderAdapter(view.context,imageList)
         viewPager_pic_two = view.findViewById<ViewPager>(R.id.idViewPager)
         viewPager_pic_two.adapter = viewPagerAdapter
     }
-    private fun getUserdata(){
-        for (i in image.indices){
-            val vidImage = VidImageModel(imageName[i],image[i])
-            VidImageList.add(vidImage)
-        }
-        vidImageAdapter= VidImageAdapter(VidImageList)
-        VidImageRV.adapter = vidImageAdapter
-        vidImageAdapter.onItemClick={
-            val intent = Intent(context,VideoPlayActivity::class.java)
-            intent.putExtra("video",it)
-            startActivity(intent)
+    private fun setupRecyclerView() {
+        if (videoAdapter == null) {
+            videoAdapter = view?.let { VideoAdapter(it.context, videoArrayList) }
+            binding?.recylerView?.layoutManager = GridLayoutManager(requireContext(), 2)
+            binding?.recylerView?.adapter = videoAdapter
+            binding?.recylerView?.itemAnimator = DefaultItemAnimator()
+            binding?.recylerView?.isNestedScrollingEnabled = true
+            videoAdapter!!.onItemClick={
+                val intent = Intent(context,VideoPlayActivity::class.java)
+                val bundle = Bundle()
+                bundle.putString("videoImage", it.urlToImage)
+                bundle.putString("video", it.videoFile?.get(0)?.url)
+                bundle.putString("author", it.user?.description)
+                bundle.putString("authorlink", it.user?.url)
+                intent.putExtras(bundle)
+                startActivity(intent)
+            }
+        } else {
+            videoAdapter!!.notifyDataSetChanged()
         }
     }
+//    private fun getUserdata(){
+//        for (i in image.indices){
+//            val vidImage = VidImageModel(imageName[i],image[i])
+//            VidImageList.add(vidImage)
+//        }
+//        vidImageAdapter= VidImageAdapter(VidImageList)
+//        VidImageRV.adapter = vidImageAdapter
+//        vidImageAdapter.onItemClick={
+//            val intent = Intent(context,VideoPlayActivity::class.java)
+//            intent.putExtra("video",it)
+//            startActivity(intent)
+//        }
+//    }
 
 }
